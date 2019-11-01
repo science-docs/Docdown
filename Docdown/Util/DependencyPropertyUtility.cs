6b9093db5e13dbfc7f61091c823429ce8c8a5a71
Mark Sujew <msujew@yahoo.de>
2019-11-01 16:08:38 +0100

    Changed auto focus on file name change

--- Docdown/Util/DependencyPropertyUtility.cs
+++ Docdown/Util/DependencyPropertyUtility.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 
 namespace Docdown.Util
@@ -8,6 +9,8 @@ namespace Docdown.Util
         public static readonly DependencyProperty CloseCommandProperty
             = DependencyProperty.RegisterAttached("CloseCommand", typeof(ICommand), typeof(DependencyPropertyUtility));
 
+        public static readonly DependencyProperty FullSelectProperty
+            = DependencyProperty.RegisterAttached("FullSelect", typeof(bool), typeof(DependencyPropertyUtility), new PropertyMetadata(false, FullSelectChanged));
         public static void SetCloseCommand(DependencyObject dependencyObject, ICommand value)
         {
             dependencyObject.SetValue(CloseCommandProperty, value);
@@ -16,6 +19,25 @@ namespace Docdown.Util
         public static ICommand GetCloseCommand(DependencyObject dependencyObject)
         {
             return (ICommand)dependencyObject.GetValue(CloseCommandProperty);
+        }
+
+        public static void SetFullSelect(DependencyObject dependencyObject, bool value)
+        {
+            dependencyObject.SetValue(FullSelectProperty, value);
+        }
+
+        public static bool GetFullSelect(DependencyObject dependencyObject)
+        {
+            return (bool)dependencyObject.GetValue(FullSelectProperty);
+        }
+
+        private static void FullSelectChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            if (obj is TextBoxBase textBox && e.NewValue is bool value && value)
+            {
+                textBox.SelectAll();
+                FocusManager.SetFocusedElement(Application.Current.MainWindow, textBox);
+            }
         }
     }
 }
