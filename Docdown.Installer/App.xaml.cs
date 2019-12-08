32b3edf9871a977508fdde7cc2f1504815ccb7f8
Mark Sujew <msujew@yahoo.de>
2019-12-08 19:07:21 +0100

    WIP Installer UI

--- Docdown.Installer/App.xaml.cs
+++ Docdown.Installer/App.xaml.cs
@@ -1,6 +1,22 @@
-﻿namespace Docdown.Installer
+﻿using MahApps.Metro;
+using System;
+using System.Windows;
+
+namespace Docdown.Installer
 {
     public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ThemeManager.AddAccent("BlueDoc", GetResourceUri("Resources/Blue.xaml"));
+            ThemeManager.AddAppTheme("DarkDoc", GetResourceUri("Resources/Dark.xaml"));
+            ThemeManager.ChangeAppStyle(Current, ThemeManager.GetAccent("BlueDoc"), ThemeManager.GetAppTheme("DarkDoc"));
+            base.OnStartup(e);
+        }
+
+        public static Uri GetResourceUri(string resource)
+        {
+            return new Uri("pack://application:,,,/Docdown.Installer;component/" + resource);
+        }
     }
 }
