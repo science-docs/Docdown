256a3ec7cc77977b2268fcb39f0fb6134e4d6d58
Mark Sujew <msujew@yahoo.de>
2019-12-12 02:12:20 +0100

    Added actual install model

---
+++ Docdown.Installer/Model.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Docdown.Installer
+{
+    public class Model
+    {
+        public bool StartupShortcut { get; set; } = true;
+        public bool DesktopShortcut { get; set; } = true;
+
+        public bool MdFileAssociation { get; set; } = true;
+        public bool BibFileAssociation { get; set; } = true;
+
+        public ICommand InstallCommand => new InternalInstallCommand(this);
+
+        public async Task Install()
+        {
+            const string name = "Docdown";
+            var userFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var docTarget = Path.Combine(userFolder, name, "Docdown.exe");
+            var names = typeof(Model).Assembly.GetManifestResourceNames();
+            await Util.Extract("Docdown.Installer.Resources.Docdown.exe", docTarget);
+            try
+            {
+                if (StartupShortcut)
+                {
+                    Shortcuts.AddStartMenuShortcut(docTarget, name);
+                }
+                if (DesktopShortcut)
+                {
+                    Shortcuts.AddDesktopShortcut(docTarget, name);
+                }
+                if (MdFileAssociation)
+                {
+                    FileAssociations.EnsureAssociationsSet(".md", "Markdown File");
+                }
+                if (BibFileAssociation)
+                {
+                    FileAssociations.EnsureAssociationsSet(".bib", "Bibliography File");
+                }
+                Process.Start(docTarget);
+                Application.Current.Dispatcher.Invoke(() => Application.Current.MainWindow.Close());
+            }
+            catch (Exception e)
+            {
+
+            }
+            
+        }
+
+        private class InternalInstallCommand : ICommand
+        {
+            public event EventHandler CanExecuteChanged;
+
+            public Model Model { get; set; }
+
+            public InternalInstallCommand(Model model)
+            {
+                Model = model;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object parameter)
+            {
+                Task.Run(Model.Install);
+            }
+        }
+    }
+}
