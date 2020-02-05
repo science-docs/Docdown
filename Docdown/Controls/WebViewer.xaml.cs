8ad28daeba753134191f5cd0cff8c5c461571c77
Mark Sujew <msujew@yahoo.de>
2020-02-05 10:46:14 +0100

    Added unloading to web viewer

--- Docdown/Controls/WebViewer.xaml.cs
+++ Docdown/Controls/WebViewer.xaml.cs
@@ -1,21 +1,38 @@
 ﻿using HtmlAgilityPack;
-using System;
+using System.Reflection;
+using System.Windows.Controls;
 
 namespace Docdown.Controls
 {
     public partial class WebViewer
     {
+        public string AfterNavigation { get; set; }
+
         public WebViewer()
         {
             InitializeComponent();
         }
 
+        public void Navigate(string url)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                Viewer.Navigate(url);
+            });
+        }
+
         public void Navigate(HtmlDocument htmlDocument)
         {
-            Dispatcher.BeginInvoke((Action)(() =>
+            Dispatcher.Invoke(() =>
             {
                 Viewer.NavigateToString(htmlDocument.Text);
-            }));
+            });
+        }
+
+        private void ViewerUnloaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // This disposes all content of the web browser
+            Viewer.Source = null;
         }
     }
 }
